d6d2ba041384bf85a8d6e6966ca3c34d5e2e0e3a
Alexandre Maia <alleh.maia@gmail.com>
2013-03-09 09:09:43 -0300

    Inicio do projeto

---
+++ PortalBrazucas2014/02 - Implementação/WEB/PortalBrazucas2014/PrototiposTelas/Controllers/TelasController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrototiposTelas.Controllers
+{
+    public class TelasController : Controller
+    {
+        //
+        // GET: /Telas/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NomeDaView()
+        {
+            return View();
+        }
+
+    }
+}
