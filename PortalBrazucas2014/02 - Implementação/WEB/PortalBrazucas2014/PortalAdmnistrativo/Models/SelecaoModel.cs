80b208d9869b49eb88b33711e1f8512b9c57dfdb
Alexandre Maia <alleh.maia@gmail.com>
2013-03-24 12:32:39 -0300

    Execução do merge

--- PortalBrazucas2014/02 - Implementação/WEB/PortalBrazucas2014/PortalAdmnistrativo/Models/SelecaoModel.cs
+++ PortalBrazucas2014/02 - Implementação/WEB/PortalBrazucas2014/PortalAdmnistrativo/Models/SelecaoModel.cs
@@ -2,34 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-<<<<<<< HEAD
-using System.ComponentModel.DataAnnotations;
-using PortalAdmnistrativo.Models.Metadata;
 
 namespace PortalAdmnistrativo.Models
 {
     public partial class Selecao
     {
-
-=======
-
-namespace PortalAdmnistrativo.Models
-{
-    public partial class SelecaoModel
-    {
->>>>>>> origin/master
         public IQueryable<Selecao> buscar()
         {
             Entities entity = new Entities();
 
             var query = entity.Selecao.AsQueryable<Selecao>();
 
-<<<<<<< HEAD
             if (this.CodigoSelecao != 0)
                 query = query.Where(item => item.CodigoSelecao == this.CodigoSelecao);
 
-=======
->>>>>>> origin/master
             return query;
         }
     }
--- PortalBrazucas2014/02 - Implementação/WEB/PortalBrazucas2014/PortalAdmnistrativo/Models/UsuarioModel.cs
+++ PortalBrazucas2014/02 - Implementação/WEB/PortalBrazucas2014/PortalAdmnistrativo/Models/UsuarioModel.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations;
-using PortalAdmnistrativo.Models.Metadata;
 
 namespace PortalAdmnistrativo.Models
 {
